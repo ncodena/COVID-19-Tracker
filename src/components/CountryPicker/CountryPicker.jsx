0212da0001c12155206d2a139fa6318255513c57
Nuria <c.odena.nuria@gmail.com>
2020-04-28 12:09:08 +0200

    displaying fetchingcountries action

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import { NativeSelect, FormControl } from '@material-ui/core';
 import styles from './CountryPicker.module.css';
+import { fetchCountries } from '../../api/index';
 
 
- const CountryPicker = ()  =>{
+ const CountryPicker = ()  => {
+
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+     
+    useEffect(() => {
+         const fetchAPI = async () => {
+            setFetchedCountries(await fetchCountries());
+         }
+
+         fetchAPI()
+     }, [setFetchedCountries]);
+
+     console.log(fetchedCountries)
+
     return (
         <FormControl className={styles.formControl}>
             <NativeSelect>
